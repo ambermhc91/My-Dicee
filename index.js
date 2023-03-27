9e178348c49afd90944bffef4d22cccb5ef5c037
ambermhc91 <122715652+ambermhc91@users.noreply.github.com>
2023-03-26 18:43:18 -0700

    added refresh btn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // var dice1Number = document.querySelector(".img1");
 // var dice2Number = document.querySelector(".img2");
-
+document.querySelector(".refresh").addEventListener("click", function(event){
+    //console.log(event);
+    window.location.reload();
+})
 var dice1Number = randomNumberGenerator();
 var dice1Img = "images/dice" + dice1Number + ".png"
 
@@ -22,6 +25,7 @@ if(dice1Number === dice2Number){
 document.querySelector(".img1").setAttribute("src", dice1Img);
 document.querySelector(".img2").setAttribute("src", dice2Img);
 document.querySelector("h1").innerHTML = message;
+//button
 
 function randomNumberGenerator(){
     var randomNumber1 = Math.random()*6;
